809c9e30a198b6e886c4ecdc3a3ff9e4dadc01b7
Ruben Santos <ruben.d.santos@tecnico.ulisboa.pt>
2019-12-13 18:20:59 +0000

    Added initial client ui.

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-'use strict';
+'use strict'
 
-const Glue = require('@hapi/glue');
+const Glue = require('@hapi/glue')
 const Manifest = require('./manifest')
 
 const options = {
   relativeTo: __dirname
-};
+}
 
 const startServer = async function () {
   try {
-    const server = await Glue.compose(Manifest, options);
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+    const server = await Glue.compose(Manifest, options)
+    await server.start()
+    console.log('Server running on %s', server.info.uri)
 
     server.route({
       method: 'GET',
       path: '/hello',
       handler: (request, h) => {
-        return 'hello world';
+        return 'hello world'
       }
-    });
-  }
-  catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-};
+    })
 
-startServer();
+    // General route
+    server.route({
+      method: 'GET',
+      path: '/{path*}',
+      handler: (request, h) => {
+
+        if (request.params.path && request.params.path.startsWith('static')) {
+          return h.file(`public/client/${request.params.path}`)
+        }
+
+        return h.file('public/client/index.html')
+      }
+    })
+
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+startServer()
---
+++ public/client/precache-manifest.1fb030a70154d11140a4c0a9b6d56e0e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cd7d11b97091b646eed27bee422f41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a8d27a0b99e62f7076f",
+    "url": "/static/css/main.365e2a42.chunk.css"
+  },
+  {
+    "revision": "d1f4aaa30b02b56f1bce",
+    "url": "/static/js/2.13954856.chunk.js"
+  },
+  {
+    "revision": "1a8d27a0b99e62f7076f",
+    "url": "/static/js/main.86ab4fd8.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  },
+  {
+    "revision": "bf9490f36cb1dec94b3fb64481db85ca",
+    "url": "/static/media/bertrand.bf9490f3.png"
+  },
+  {
+    "revision": "a466e0d8e05d57b4e8060b1cd80d27be",
+    "url": "/static/media/bookdep.a466e0d8.png"
+  },
+  {
+    "revision": "16a85765b646ff39705769ea6be9ec67",
+    "url": "/static/media/header.16a85765.png"
+  },
+  {
+    "revision": "4506397f97d9d255bb001b663524e903",
+    "url": "/static/media/loading.4506397f.gif"
+  },
+  {
+    "revision": "e0c1a39c340b63761103a2b9bd819d25",
+    "url": "/static/media/loading2.e0c1a39c.gif"
+  },
+  {
+    "revision": "330a824939ddf16aac61dfd78822d6fc",
+    "url": "/static/media/wook.330a8249.jpg"
+  }
+]);
