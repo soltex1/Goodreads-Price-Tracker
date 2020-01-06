bfe538e816e2f1029c3a053078df8d117689082b
Ruben Santos <ruben.d.santos@tecnico.ulisboa.pt>
2020-01-06 19:37:54 +0000

    Added new errors to the error handler.

--- constants/requestErrors.js
+++ constants/requestErrors.js
@@ -1,8 +1,12 @@
-const TRANSFORM_ERROR = 'TransformError';
-const STATUS_CODE_ERROR = 'StatusCodeError';
-const SERVER_ERROR = 'ServerError';
+const TRANSFORM_ERROR = 'TransformError'
+const STATUS_CODE_ERROR = 'StatusCodeError'
+const SERVER_ERROR = 'ServerError'
+const JSON_CONVERSION_ERROR = 'JsonConversionError'
+const JSON_PARSING_ERROR = 'JsonParsingError'
 
 module.exports = {
+  JSON_CONVERSION_ERROR,
+  JSON_PARSING_ERROR,
   TRANSFORM_ERROR,
   STATUS_CODE_ERROR,
   SERVER_ERROR
