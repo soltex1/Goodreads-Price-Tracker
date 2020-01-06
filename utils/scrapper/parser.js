35bf1a92ec61f66d5d86e6e4ae22ae5bd88e14eb
Ruben Santos <ruben.d.santos@tecnico.ulisboa.pt>
2020-01-06 19:45:58 +0000

    Removed try/catch (the high order function catches the errors), added a new error description and fixed the meta, send an empty object if it can't find the key.

--- utils/scrapper/parser.js
+++ utils/scrapper/parser.js
@@ -5,18 +5,17 @@ const ErrorHandler = require('../../utils/errorHandler')
 const internals = {}
 
 /**
- * Converts a given json into another json with a different format.
+ * Converts a json into another json with a different format.
  * @param json
  * @returns {Object}
  */
 internals.parser = async (json) => {
 
-  try {
     const { GoodreadsResponse } = json
 
     // Check if the json contains the books
     if (!GoodreadsResponse && !GoodreadsResponse.books[0].book) {
-      return new Error()
+      throw new Error('JsonParsingError')
     }
 
     // Books
@@ -28,18 +27,13 @@ internals.parser = async (json) => {
     // Pagination
     const meta = goodReadsBooks.$
       ? {
-        currentPage: parseInt(goodReadsBooks.$.currentpage),
-        numPages: parseInt(goodReadsBooks.$.numpages),
-        totalItems: parseInt(goodReadsBooks.$.total)
+        currentPage: parseInt(goodReadsBooks.$.currentpage) || null,
+        numPages: parseInt(goodReadsBooks.$.numpages) || null,
+        totalItems: parseInt(goodReadsBooks.$.total) || null
       }
       : {}
 
     return { books, meta }
-
-  } catch (e) {
-    // Handle the errors
-    return ErrorHandler(e)
-  }
 }
 
 /**
@@ -47,14 +41,13 @@ internals.parser = async (json) => {
  * @param book
  * @returns {Object}
  */
+internals.bookParser = (book) => {
 
-// parse a book in json format from good reads
-internals.bookParser = async (book) => {
   const { id, isbn, isbn13, title, image_url, authors } = book
 
   const author = authors[0] && authors[0].author[0] ? authors[0].author[0].name[0] : null
-  const numPages = book.num_pages ? parseFloat(book.num_pages[0]) : null
-  const avgRating = book.average_rating ? parseFloat(book.average_rating[0]) : null
+  const numPages = parseFloat(book.num_pages[0]) || null
+  const avgRating = parseFloat(book.average_rating[0]) || null
 
   const bookId = id[0]['_']
 
@@ -68,8 +61,10 @@ internals.bookParser = async (book) => {
     numPages,
     avgRating,
     prices: {
-      WOOK: null,
-      BOOK_DEPOSITORY: null
+      bertrand: null,
+      book_depository: null,
+      fnac: null,
+      wook: null,
     }
   }
 }
