bf730c0998741fa12747db0a1dfdb8097e688070
Ruben Santos <ruben.d.santos@tecnico.ulisboa.pt>
2020-01-06 19:43:23 +0000

    Added new errors to the error handler.

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,6 +1,8 @@
 // Imports
 const Boom = require('@hapi/boom')
 const {
+  JSON_CONVERSION_ERROR,
+  JSON_PARSING_ERROR,
   SERVER_ERROR,
   STATUS_CODE_ERROR,
   TRANSFORM_ERROR
@@ -21,6 +23,14 @@ internals.errorHandler = (error) => {
     case STATUS_CODE_ERROR:
       // The request failed due status code error
       return Boom.notFound(STATUS_CODE_ERROR)
+
+    case JSON_CONVERSION_ERROR:
+      // The request failed due conversion to json error
+      return Boom.preconditionFailed(JSON_CONVERSION_ERROR)
+
+    case JSON_PARSING_ERROR:
+      // The request failed due json parsing error
+      return Boom.preconditionFailed(JSON_PARSING_ERROR)
 
     default:
       // The request failed due to technical reasons.
