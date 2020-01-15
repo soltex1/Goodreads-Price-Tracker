687d82d7626ea431e69d793ac07ed8c2aa362ffc
Ruben Santos <ruben.d.santos@tecnico.ulisboa.pt>
2020-01-15 17:18:16 +0000

    Improved fnac tracker.

--- utils/trackers/fnac.js
+++ utils/trackers/fnac.js
@@ -1,6 +1,6 @@
 // Imports
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
 
 // Declare internals
 const internals = {}
@@ -13,22 +13,31 @@ const internals = {}
  */
 internals.fnacParser = (body, response) => {
 
+  const prices = []
+
   if (response.statusCode === 200) {
 
     // Convert the body into DOM elements
-    const dom = new JSDOM(body);
+    const dom = new JSDOM(body)
 
-    // Get the price from the DOM element
-    if (dom.window.document.getElementsByClassName('userPrice').length) {
-      const price = dom.window.document.getElementsByClassName('userPrice')[0].innerHTML
+    let minPrice = Number.MAX_SAFE_INTEGER;
 
-      if (price !== null) {
-        return parseFloat(price.replace(',', '.'))
+    ['userPrice', 'price red'].forEach((className) => {
+
+      // Get the price from the DOM element
+      if (dom.window.document.getElementsByClassName(className).length) {
+        const price = dom.window.document.getElementsByClassName(className)[0].innerHTML
+
+        if (price !== null) {
+          let currentPrice = parseFloat(price.replace(',', '.'))
+
+          if (!isNaN(currentPrice)) prices.push(currentPrice)
+        }
       }
-    }
+    })
   }
 
-  return 'N/A'
+  return prices.length ? Math.min.apply(null, prices) : 'N/A'
 }
 
 module.exports = internals.fnacParser
